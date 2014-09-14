9f6f74a9b7df789cfe2d82fc2357af1ea248de5a
Konemiees <konsta.sinisalo@gmail.com>
2014-09-15 00:32:33 +0300

    Kamera seuraa kuin poliisikoira

---
+++ Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 s;
+	private Vector3 c;
+
+	private float skin =.005f;
+
+	[HideInInspector]
+	public bool grounded;
+	[HideInInspector]
+	public bool movementStopped;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start (){
+		collider = GetComponent<BoxCollider> ();
+		s = collider.size;
+		c = collider.center;
+	}
+
+	public void Move(Vector2 moveAmount){
+
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 p = transform.position;
+
+		grounded = false;
+		for (int i = 0; i<3; i++) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + c.x - s.x/2)+s.x/2*i;
+			float y = p.y+c.y+s.y/2*dir;
+
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+			Debug.DrawRay(ray.origin, ray.direction);
+
+			if(Physics.Raycast(ray, out hit, Mathf.Abs(deltaY) + skin ,collisionMask)){
+				float dst = Vector3.Distance (ray.origin, hit.point);
+
+				if(dst > skin){
+					deltaY = dst * dir - skin * dir;
+				}
+				else{
+					deltaY = 0;
+				}
+				grounded = true;
+				break;
+			}
+		}
+
+		//Left and right collission
+
+		movementStopped = false;
+		for (int i = 0; i<3; i++) {
+			float dir = Mathf.Sign(deltaX);
+			float x = p.x + c.x + s.x/2*dir;
+			float y = p.y+c.y-s.y/2 + s.y/2*i;
+			
+			ray = new Ray(new Vector2(x,y), new Vector2(dir, 0));
+			Debug.DrawRay(ray.origin, ray.direction);
+			
+			if(Physics.Raycast(ray, out hit, Mathf.Abs(deltaX) + skin ,collisionMask)){
+				float dst = Vector3.Distance (ray.origin, hit.point);
+				
+				if(dst > skin){
+					deltaX = dst * dir - skin * dir;
+				}
+				else{
+					deltaX = 0;
+				}
+				movementStopped = true;
+				break;
+			}
+			
+			
+		}
+
+		Vector2 finalTransform = new Vector2 (deltaX, deltaY);
+		transform.Translate (finalTransform);
+	}
+
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameManager : MonoBehaviour {
+
+	public GameObject player;
+	private gameCamera cam;
+	
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<gameCamera>();
+		spawnPlayer();
+	}
+
+	private void spawnPlayer(){
+		cam.setTarget((Instantiate (player, Vector3.zero, Quaternion.identity)as GameObject).transform);
+	}
+
+
+}
